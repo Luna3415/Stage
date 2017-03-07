4ff89ae8572ea107f74726b420303b32603e5827
Kim <kim.gijbels@student.pxl.be>
2017-03-07 18:02:05 +0100

    Integrated chessEngine in service

--- src/main/java/be/pxl/kim/serviceImpls/ChessGameService.java
+++ src/main/java/be/pxl/kim/serviceImpls/ChessGameService.java
@@ -2,16 +2,24 @@ package be.pxl.kim.serviceImpls;
 
 import org.springframework.stereotype.Service;
 
+import be.pxl.kim.chessengine.Stockfish;
 import be.pxl.kim.models.ChessGame;
 import be.pxl.kim.services.IChessGameService;
 
 @Service
 public class ChessGameService implements IChessGameService{
 
-	@Override
 	public String calculateBestMove(ChessGame game) {
-		// TODO Auto-generated method stub
-		return "It works";
+		Stockfish chessEngine = new Stockfish();
+		
+		if (chessEngine.startEngine()) {
+			System.out.println("Engine has started..");
+		} else {
+			System.out.println("Oops! Something went wrong..");
+		}
+		chessEngine.getOutput(0);
+		String move = chessEngine.getBestMoveWithWaitTime(game.getFen(), game.getWaitTime());
+		return move;
 	}
 
 }
